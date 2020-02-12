856e85b2e21ae3b20e3a52d32b59083e3f2b24ab
ericchen321 <cgx1997@outlook.com>
2020-02-11 22:23:00 -0800

    classes for various shapes

---
+++ CAD_with_Neo_smartpen/Shapes.py
@@ -0,0 +1,34 @@
+'''
+Created on 2020-02-11
+
+@author: Guanxiong
+'''
+
+class Circle():
+    def __init__(self, center_x, center_y, radius):
+        self.center_x = center_x
+        self.center_y = center_y
+        self.radius = radius
+
+class Rectangle():
+    def __init__(self, upperleft_x, upperleft_y, center_x, center_y):
+        self.upperleft_x = upperleft_x
+        self.upperleft_y = upperleft_y
+        self.center_x = center_x
+        self.center_y = center_y
+
+class Triangle():
+    def __init__(self, x_0, y_0, x_1, y_1, x_2, y_2):
+        self.x_0 = x_0
+        self.y_0 = y_0
+        self.x_1 = x_1
+        self.y_1 = y_1
+        self.x_2 = x_2
+        self.y_2 = y_2
+
+class Line():
+    def __Init__(self, x_0, y_0, x_1, y_1):
+        self.x_0 = x_0
+        self.y_0 = y_0
+        self.x_1 = x_1
+        self.y_1 = y_1
